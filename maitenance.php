27b73c9662308c744f5fe32bca8f302cedd3102d
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2023-05-15 19:54:36 +0100

    any sin

---
+++ maitenance.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <link rel="icon" type="image/png" href="logo.ico">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    <title>LUULUILUI - Mode maintenance</title>
+</head>
+
+<body>
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <img src="logonoir.png" class="mx-auto" width=250 alt="">
+            </div>
+
+            <h1>
+                MODE MAINTENANCE JUSQU'AU 22/05/2023
+            </h1> <br>
+
+            <p class="text">
+                Nous nous excusons pour la gêne occasionnée.
+                Le site est actuellement en cours de
+                maintenance pour améliorer votre expérience.
+                Nous travaillons activement pour
+                le remettre en ligne.
+                Merci de votre patience et de votre compréhension.
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
